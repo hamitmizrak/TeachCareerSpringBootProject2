280e4db0177fb289161932f5d9d76544cab8c83b
hamitmizrak <hamitmizrak@gmail.com>
2022-07-06 18:20:24 +0300

    Initial commit

---
+++ src/main/java/com/hamitmizrak/TeachCareerSpringBootProject2Application.java
@@ -0,0 +1,27 @@
+package com.hamitmizrak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//exclude:dahil etmemek
+
+@SpringBootApplication(exclude = {
+        org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class,
+        org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration.class
+}
+)
+public class TeachCareerSpringBootProject2Application {
+
+    public static void main(String[] args) {
+
+
+        //DevTools
+        System.setProperty("spring.devtools.restart.enabled","false");
+
+        // AWT: JOptionPanel set ayarı
+        System.setProperty("java.awt.headless", "false");
+
+        SpringApplication.run(TeachCareerSpringBootProject2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/hamitmizrak/TeachCareerSpringBootProject2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hamitmizrak;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeachCareerSpringBootProject2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
